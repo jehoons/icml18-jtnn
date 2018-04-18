9283b7a01ea449f63b3723fe3db9347bfd3ec3e5
leswing <lilleswing@gmail.com>
2018-04-18 09:03:46 -0400

    python 3

--- jtnn/datautils.py
+++ jtnn/datautils.py
@@ -1,37 +1,59 @@
 from torch.utils.data import Dataset
 from .mol_tree import MolTree
 import numpy as np
+import random
+
 
 class MoleculeDataset(Dataset):
 
-    def __init__(self, data_file):
-        with open(data_file) as f:
-            self.data = [line.strip("\r\n ").split()[0] for line in f]
+  def __init__(self, data_file):
+    with open(data_file) as f:
+      self.data = [line.strip("\r\n ").split()[0] for line in f]
 
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        smiles = self.data[idx]
-        mol_tree = MolTree(smiles)
-        mol_tree.recover()
-        mol_tree.assemble()
-        return mol_tree
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+    smiles = self.data[idx]
+    mol_tree = MolTree(smiles)
+    mol_tree.recover()
+    mol_tree.assemble()
+    return mol_tree
+
 
 class PropDataset(Dataset):
 
-    def __init__(self, data_file, prop_file):
-        self.prop_data = np.loadtxt(prop_file)
-        with open(data_file) as f:
-            self.data = [line.strip("\r\n ").split()[0] for line in f]
+  def __init__(self, data_file, prop_file):
+    self.prop_data = np.loadtxt(prop_file)
+    with open(data_file) as f:
+      self.data = [line.strip("\r\n ").split()[0] for line in f]
 
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        smiles = self.data[idx]
-        mol_tree = MolTree(smiles)
-        mol_tree.recover()
-        mol_tree.assemble()
-        return mol_tree, self.prop_data[idx]
+  def __len__(self):
+    return len(self.data)
 
+  def __getitem__(self, idx):
+    smiles = self.data[idx]
+    mol_tree = MolTree(smiles)
+    mol_tree.recover()
+    mol_tree.assemble()
+    return mol_tree, self.prop_data[idx]
+
+
+class FastMoleculeDataset(Dataset):
+  # Same as MoleculeDataset but pickle results so its not so GD slow
+
+  def __init__(self, data_file, shuffle=False):
+    with open(data_file) as f:
+      self.data = [line.strip("\r\n ").split()[0] for line in f]
+    if shuffle:
+      random.shuffle(self.data)
+
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+    smiles = self.data[idx]
+    mol_tree = MolTree(smiles)
+    mol_tree.recover()
+    mol_tree.assemble()
+    return mol_tree
--- molvae/pretrain.py
+++ molvae/pretrain.py
@@ -12,7 +12,7 @@ from collections import deque
 from jtnn import *
 import rdkit
 
-lg = rdkit.RDLogger.logger() 
+lg = rdkit.RDLogger.logger()
 lg.setLevel(rdkit.RDLogger.CRITICAL)
 
 parser = OptionParser()
@@ -23,9 +23,9 @@ parser.add_option("-b", "--batch", dest="batch_size", default=40)
 parser.add_option("-w", "--hidden", dest="hidden_size", default=200)
 parser.add_option("-l", "--latent", dest="latent_size", default=56)
 parser.add_option("-d", "--depth", dest="depth", default=3)
-opts,args = parser.parse_args()
-   
-vocab = [x.strip("\r\n ") for x in open(opts.vocab_path)] 
+opts, args = parser.parse_args()
+
+vocab = [x.strip("\r\n ") for x in open(opts.vocab_path)]
 vocab = Vocab(vocab)
 
 batch_size = int(opts.batch_size)
@@ -36,10 +36,10 @@ depth = int(opts.depth)
 model = JTNNVAE(vocab, hidden_size, latent_size, depth)
 
 for param in model.parameters():
-    if param.dim() == 1:
-        nn.init.constant(param, 0)
-    else:
-        nn.init.xavier_normal(param)
+  if param.dim() == 1:
+    nn.init.constant(param, 0)
+  else:
+    nn.init.xavier_normal(param)
 
 model = model.cuda()
 print("Model #Params: %dK" % (sum([x.nelement() for x in model.parameters()]) / 1000,))
@@ -54,38 +54,39 @@ MAX_EPOCH = 3
 PRINT_ITER = 20
 
 for epoch in range(MAX_EPOCH):
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=lambda x:x, drop_last=True)
+  dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4, collate_fn=lambda x: x,
+                          drop_last=True)
 
-    word_acc,topo_acc,assm_acc,steo_acc = 0,0,0,0
+  word_acc, topo_acc, assm_acc, steo_acc = 0, 0, 0, 0
 
-    for it, batch in enumerate(dataloader):
-        for mol_tree in batch:
-            for node in mol_tree.nodes:
-                if node.label not in node.cands:
-                    node.cands.append(node.label)
-                    node.cand_mols.append(node.label_mol)
+  for it, batch in enumerate(dataloader):
+    for mol_tree in batch:
+      for node in mol_tree.nodes:
+        if node.label not in node.cands:
+          node.cands.append(node.label)
+          node.cand_mols.append(node.label_mol)
 
-        model.zero_grad()
-        loss, kl_div, wacc, tacc, sacc, dacc = model(batch, beta=0)
-        loss.backward()
-        optimizer.step()
+    model.zero_grad()
+    loss, kl_div, wacc, tacc, sacc, dacc = model(batch, beta=0)
+    loss.backward()
+    optimizer.step()
 
-        word_acc += wacc
-        topo_acc += tacc
-        assm_acc += sacc
-        steo_acc += dacc
+    word_acc += wacc
+    topo_acc += tacc
+    assm_acc += sacc
+    steo_acc += dacc
 
-        if (it + 1) % PRINT_ITER == 0:
-            word_acc = word_acc / PRINT_ITER * 100
-            topo_acc = topo_acc / PRINT_ITER * 100
-            assm_acc = assm_acc / PRINT_ITER * 100
-            steo_acc = steo_acc / PRINT_ITER * 100
+    if (it + 1) % PRINT_ITER == 0:
+      word_acc = word_acc / PRINT_ITER * 100
+      topo_acc = topo_acc / PRINT_ITER * 100
+      assm_acc = assm_acc / PRINT_ITER * 100
+      steo_acc = steo_acc / PRINT_ITER * 100
 
-            print("KL: %.1f, Word: %.2f, Topo: %.2f, Assm: %.2f, Steo: %.2f" % (kl_div, word_acc, topo_acc, assm_acc, steo_acc))
-            word_acc,topo_acc,assm_acc,steo_acc = 0,0,0,0
-            sys.stdout.flush()
+      print(
+        "KL: %.1f, Word: %.2f, Topo: %.2f, Assm: %.2f, Steo: %.2f" % (kl_div, word_acc, topo_acc, assm_acc, steo_acc))
+      word_acc, topo_acc, assm_acc, steo_acc = 0, 0, 0, 0
+      sys.stdout.flush()
 
-    scheduler.step()
-    print("learning rate: %.6f" % scheduler.get_lr()[0])
-    torch.save(model.state_dict(), opts.save_path + "/model.iter-" + str(epoch))
-
+  scheduler.step()
+  print("learning rate: %.6f" % scheduler.get_lr()[0])
+  torch.save(model.state_dict(), opts.save_path + "/model.iter-" + str(epoch))
